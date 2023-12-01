1251925c28efd496292bbcaa0f42dbe427d2b250
sergio15dev <sergio15.dev@gmail.com>
2023-11-30 22:01:35 -0500

    add first settings spring security

---
+++ ferreteria/src/main/java/com/tecsup/ferreteria/users/UserRepository.java
@@ -0,0 +1,14 @@
+package com.unsa.backend.users;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long>{
+    Optional<UserModel> findByUsername(String username);
+    boolean existsByUsername(String username);
+    Optional<UserModel> findById(Long id);
+}
